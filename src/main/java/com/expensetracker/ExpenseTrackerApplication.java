28ac7e73145ca90901cf23208678ebedcf701ad1
Nurdiyar <nurdi.instagramm@gmail.com>
2025-09-23 14:59:47 +0500

    Added Spring Boot dependencies. Update ExpenseTrackerApplication with @SpringBootApplication.

--- src/main/java/com/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/expensetracker/ExpenseTrackerApplication.java
@@ -1,10 +1,14 @@
 package com.expensetracker;
 
 import com.expensetracker.controller.ControllerOfAllApp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ExpenseTrackerApplication {
     public static void main(String[] args) {
-        ControllerOfAllApp controllerOfAllApp = new ControllerOfAllApp();
-        controllerOfAllApp.start();
+        SpringApplication.run(ExpenseTrackerApplication.class, args);
+//        ControllerOfAllApp controllerOfAllApp = new ControllerOfAllApp();
+//        controllerOfAllApp.start();
     }
 }
