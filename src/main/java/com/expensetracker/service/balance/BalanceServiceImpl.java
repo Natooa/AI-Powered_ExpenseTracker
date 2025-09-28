3618e2a87371859b043e9e33b5e41743f4562c06
Nurdiyar <nurdi.instagramm@gmail.com>
2025-09-28 20:32:25 +0500

    feat(balance): add BalanceDTO, service implementation and controller with /balance endpoint

--- src/main/java/com/expensetracker/service/balance/BalanceServiceImpl.java
+++ src/main/java/com/expensetracker/service/balance/BalanceServiceImpl.java
@@ -1,30 +1,47 @@
-//package com.expensetracker.service.balance;
-//
-//import com.expensetracker.entity.Expense;
-//import com.expensetracker.entity.Income;
-//import com.expensetracker.service.transaction.ExpenseServiceImpl;
-//import com.expensetracker.service.transaction.IncomeServiceImpl;
-//import org.springframework.stereotype.Service;
-//import java.math.BigDecimal;
-//
-//@Service
-//public class BalanceServiceImpl implements BalanceService {
-//    private final IncomeServiceImpl incomeService;
-//    private final ExpenseServiceImpl expenseService;
-//
-//    public BalanceServiceImpl(IncomeServiceImpl incomeService, ExpenseServiceImpl expenseService) {
-//        this.incomeService = incomeService;
-//        this.expenseService = expenseService;
-//    }
-//
-//    @Override
-//    public BigDecimal getBalance() {
-//        BigDecimal incomeSum = incomeService.getAllTransactions().values().stream().map(Income::getAmount)
-//                .reduce(BigDecimal.ZERO, BigDecimal::add);
-//
-//        BigDecimal expenseSum = expenseService.getAllTransactions().values().stream().map(Expense::getAmount)
-//                .reduce(BigDecimal.ZERO, BigDecimal::add);
-//
-//        return incomeSum.subtract(expenseSum);
-//    }
-//}
+package com.expensetracker.service.balance;
+
+import com.expensetracker.dto.BalanceDTO;
+import com.expensetracker.entity.Expense;
+import com.expensetracker.entity.Income;
+import com.expensetracker.service.transaction.ExpenseServiceImpl;
+import com.expensetracker.service.transaction.IncomeServiceImpl;
+import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+
+@Service
+public class BalanceServiceImpl implements BalanceService {
+    private final IncomeServiceImpl incomeService;
+    private final ExpenseServiceImpl expenseService;
+
+    public BalanceServiceImpl(IncomeServiceImpl incomeService, ExpenseServiceImpl expenseService) {
+        this.incomeService = incomeService;
+        this.expenseService = expenseService;
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return getBalanceDTO().getTotalBalance();
+    }
+
+    @Override
+    public BigDecimal getTotalIncome() {
+        return getBalanceDTO().getTotalIncome();
+    }
+
+    @Override
+    public BigDecimal getTotalExpense() {
+        return getBalanceDTO().getTotalExpense();
+    }
+
+    @Override
+    public BalanceDTO  getBalanceDTO() {
+        BigDecimal income = incomeService.getAllTransactions().stream()
+                .map(Income::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal expense = expenseService.getAllTransactions().stream()
+                .map(Expense::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal totalBalance = income.subtract(expense);
+        return new BalanceDTO(income, expense, totalBalance);
+    }
+}
